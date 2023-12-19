a899e2e6a3fd7352863ba2559853cec98302d4a5
Aleksandr Eskov <mullikaloll@yandex.ru>
2023-12-19 23:44:50 +0500

    bam bam

--- bot.py
+++ bot.py
@@ -1,11 +1,25 @@
 from aiogram import Bot
-from aiogram import Router
-
 
 BOT_TOKEN = "5937470704:AAEmI-qYRIRAihYAhWY6rT6i8BUPu1lwiNk"
+CHAT_ID = -1002146063171
 
-user_router = Router()
+bot = Bot(BOT_TOKEN)
 
-@user_router()
-async def new_lead():
-    pass
+
+def get_message_text(parsed_info: dict):
+    return f"ID: {parsed_info.get('id')}\n" \
+           f"Имя контакта: {parsed_info.get('name')}\n" \
+           f"Комментарий: {parsed_info.get('comments')}\n" \
+           f"Телефон: {parsed_info.get('phone')}\n" \
+           f"Оценка площадки: {parsed_info.get('place_score')}"
+
+
+async def send_message(parsed_info: dict):
+    """
+    Отправка сообщения в канал
+
+    :param parsed_info:
+    :return:
+    """
+    message_text = get_message_text(parsed_info)
+    await bot.send_message(CHAT_ID, message_text)
--- web.py
+++ web.py
@@ -1,9 +1,42 @@
-from typing import Any
 import uvicorn
+from urllib.parse import parse_qs
 from fastapi import FastAPI, Request
-from pydantic import BaseModel
+import requests
+
+from bot import send_message
+
 app = FastAPI()
 BITRIX_TOKEN = "ueqd3rweu1k5z52xnyk5zcr17m3zoxqy"
+
+PLACE_SCORE = {
+    "1883": 1,
+    "1885": 2,
+    "1887": 3,
+    "1889": 4,
+    "1891": 5
+}
+
+RECOMMENDATION = {
+    "1903": "Определенно Да",
+    "1905": "Скорее всего да",
+    "1907": "Скорее всего нет",
+    "1909": "Определенно нет"
+}
+
+HOLIDAY = {
+    "1919": "В восторге! Праздник провели на высшем уровне",
+    "1921": "Все понравилось, но были недочеты",
+    "1923": "Не понравилось",
+    "1925": "Не проводили еще праздники в Scandy Park"
+}
+
+PERSONAL_SCORE = {
+    "1927": 1,
+    "1929": 2,
+    "1931": 3,
+    "1933": 4,
+    "1935": 5
+}
 
 
 @app.post("/lead")
@@ -14,10 +47,57 @@ async def get_new_lead(request: Request):
     """
     print("Зашли сюда")
     try:
-        data = await request.body()
-        print(data)
+        response = await request.body()
+        decoded_response = response.decode("utf-8")
+
+        data = parse_qs(decoded_response)
+
+        lid_ids: list[str] = data.get("data[FIELDS][ID]")
+
+        if not lid_ids:
+            return
+
+        lid_id = lid_ids[0]
+        print(lid_id)
+        lead_info = get_lid_info(lid_id)
+        result = lead_info.get("result")
+        if not result or result.get("STATUS_ID") != "20":
+            return
+        parsed_info = parse_lid_info(lead_info)
+        await send_message(parsed_info)
+
     except:
         print("Не смогли достать json")
+
+
+def get_lid_info(lid_id: str):
+    """
+
+    :param lid_id:
+    :return:
+    """
+    link = f"https://lasvegas.bitrix24.ru/rest/47181/3eggrep7any8rvue/crm.lead.get?id={lid_id}"
+    try:
+        return requests.get(link).json()
+    except:
+        print("Не пошло")
+
+
+def parse_lid_info(lid_info: dict):
+    result = lid_info.get("result")
+
+    return {
+        "place_score": PLACE_SCORE.get(result.get("UF_CRM_1694005680")),
+        "recommendation": result.get("UF_CRM_1694005757"),
+        "holiday": result.get("UF_CRM_1694005820"),
+        "personal_score": result.get("UF_CRM_1694005866"),
+        "attraction_score": result.get("UF_CRM_1694005680"),
+        "id": result.get("ID"),
+        "name": result.get("NAME"),
+        "comments": result.get("comments"),
+        "phone": result.get("phone")
+    }
+
 
 if __name__ == "__main__":
     try:
